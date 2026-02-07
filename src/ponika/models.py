9ea3a5ea5f0849b7f977f6159bdc2210deea24c3
Stefan Warnat <ich@stefanwarnat.de>
2026-02-07 23:40:50 +0100

    feat: implement BasePayload class

--- src/ponika/models.py
+++ src/ponika/models.py
@@ -1,10 +1,17 @@
-from typing import Generic, List, Optional, TypeVar
+from dataclasses import asdict, dataclass
+from enum import Enum
+from typing import TYPE_CHECKING, Generic, List, Optional, TypeVar
+
+if TYPE_CHECKING:
+    from ponika import PonikaClient
+
 from pydantic import BaseModel
 
 
 T = TypeVar("T")
 
 
+    
 class TeltonikaApiError(BaseModel):
     """Custom exception for Teltonika API errors."""
 
@@ -17,12 +24,32 @@ class TeltonikaApiError(BaseModel):
         return f"Error {self.code}: {self.error} ({self.source}, {self.section})"
 
 
+class Endpoint:
+    def __init__(self, client: "PonikaClient") -> None:
+        self._client: "PonikaClient" = client    
+
 class ApiResponse(BaseModel, Generic[T]):
     success: bool
     data: None | T = None
     errors: Optional[List[TeltonikaApiError]] = None
 
+@dataclass
+class BasePayload:
+    def asdict(self) -> dict:
+        def convert(value):
+            if isinstance(value, Enum):
+                return value.value
+            if isinstance(value, list):
+                return [convert(v) for v in value]
+            if isinstance(value, bool):
+                return "1" if value == True else "0"
+            if isinstance(value, dict):
+                return {k: convert(v) for k, v in value.items()}
+            
+            return value
 
+        return convert(asdict(self))    
+    
 class Token(BaseModel):
     """Data model for token storage."""
 

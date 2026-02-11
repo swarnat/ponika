2ff1d78c54137e1b9cc13d210e74fe16f78cf2e7
Stefan Warnat <ich@stefanwarnat.de>
2026-02-11 23:09:48 +0100

    feat: add users endpoints refactor: remove old dhcp endpoint file

---
+++ src/ponika/endpoints/users.py
@@ -0,0 +1,48 @@
+from dataclasses import dataclass
+from typing import TYPE_CHECKING, Optional, Type
+from ponika.endpoints import CRUDEndpoint
+from ponika.models import BaseModel, BasePayload
+
+if TYPE_CHECKING:
+    from ponika import PonikaClient
+
+
+class UserDeleteResponse(BaseModel):
+    id: str
+
+
+class UserDefinition(BasePayload):
+    username: str
+    password: str
+    group: str
+    ssh_enable: bool = False
+
+
+class UserConfigResponse(BaseModel):
+    id: str
+    username: str
+    group: str
+    ssh_enable: bool
+
+
+class UserUpdateDefinition(BasePayload):
+    id: str
+    group: Optional[str] = ""
+    ssh_enable: Optional[bool] = False
+
+    current_password: Optional[str] = None
+    password: Optional[str] = None
+    password_confirm: Optional[str] = None
+
+
+class UsersEndpoint(
+    CRUDEndpoint[UserDefinition, UserConfigResponse,
+                 UserUpdateDefinition, UserDeleteResponse]
+):
+    endpoint_path = "/users/config"
+
+    create_model = UserDefinition
+    update_model = UserUpdateDefinition
+
+    config_response_model = UserConfigResponse
+    delete_reponse_model = UserDeleteResponse

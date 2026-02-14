74d3af856336d8ba2d56ce26a9c251ab2c00cc48
Stefan Warnat <ich@stefanwarnat.de>
2026-02-14 14:16:01 +0100

    feat: extend TailScale endpoints

--- src/ponika/endpoints/tailscale.py
+++ src/ponika/endpoints/tailscale.py
@@ -1,47 +1,48 @@
 from typing import List, Literal, Optional
-from pydantic import BaseModel
-from typing import TYPE_CHECKING
 
-from ponika.models import ApiResponse
-
-if TYPE_CHECKING:
-    from ponika import PonikaClient
+from ponika.endpoints import Endpoint, ReadEndpoint, StatusEndpoint, UpdateEndpoint
+from ponika.models import BaseModel, BasePayload
 
 
-class TailscaleEndpoint:
-    def __init__(self, client: "PonikaClient") -> None:
-        self._client: "PonikaClient" = client
+class TailscaleConfigBase:
+    enabled: Optional[str] = None
+    auth_key: Optional[str] = None
+    advert_routes: Optional[List[str]] = None
+    accept_routes: Optional[str] = None
+    exit_node: Optional[str] = None
+    auth_type: Literal["url", "key"]
+    default_route: Optional[Literal["0", "1"]] = None
+    exit_node_ip: Optional[str] = None
+    login_server: Optional[str] = None
 
-    class TailscaleConfigResponseDataItem(BaseModel):
-        """Data model for Tailscale configuration response."""
 
-        id: Optional[str] = None
-        enabled: Optional[str] = None
-        auth_key: Optional[str] = None
-        advert_routes: Optional[List[str]] = None
-        accept_routes: Optional[str] = None
-        exit_node: Optional[str] = None
-        auth_type: Literal["url", "key"]
-        default_route: Optional[Literal["0", "1"]] = None
-        exit_node_ip: Optional[str] = None
-        login_server: Optional[str] = None
+class TailscaleConfigResponse(BaseModel, TailscaleConfigBase):
+    id: Optional[str] = None
 
-    def get_config(self) -> "ApiResponse[List[TailscaleConfigResponseDataItem]]":
-        """Fetch Tailscale configuration from the device."""
-        return ApiResponse[List[self.TailscaleConfigResponseDataItem]].model_validate(
-            self._client._get("/tailscale/config")
-        )
 
-    class TailscaleStatusResponseDataItem(BaseModel):
-        """Data model for Tailscale status response."""
+class TailscaleConfigUpdatePayload(BasePayload, TailscaleConfigBase):
+    """Payload for both single-item and bulk Tailscale updates."""
 
-        status: str
-        url: str
-        ip: List[str]
-        message: List[str]
 
-    def get_status(self) -> "ApiResponse[List[TailscaleStatusResponseDataItem]]":
-        """Fetch Tailscale status from the device."""
-        return ApiResponse[List[self.TailscaleStatusResponseDataItem]].model_validate(
-            self._client._get("/tailscale/status")
-        )
+class TailscaleStatusResponse(BaseModel):
+    status: str
+    url: str
+    ip: List[str]
+    message: List[str]
+
+
+class TailscaleEndpoint(
+    Endpoint,
+    ReadEndpoint[TailscaleConfigResponse],
+    UpdateEndpoint[TailscaleConfigUpdatePayload, TailscaleConfigResponse],
+    StatusEndpoint[TailscaleStatusResponse],
+):
+    endpoint_path = "/tailscale/config"
+    status_endpoint_path = "/tailscale/status"
+    allow_status_with_id = False
+    allow_bulk_update = True
+
+    config_response_model = TailscaleConfigResponse
+    update_model = TailscaleConfigUpdatePayload
+    status_response_model = TailscaleStatusResponse
+

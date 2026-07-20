30aa82025eaa1b1be77ed3686f459cc48df15427
Stefan Warnat <ich@stefanwarnat.de>
2026-07-20 16:41:42 +0200

    docs: show how to update user password

---
+++ examples/users/users_set_password.py
@@ -0,0 +1,31 @@
+from examples.config import connection
+
+response = connection.users.get_config()
+
+update_user = None
+
+for user in response:
+    # if user.username != 'admin':
+    update_user = user
+    # break
+
+if update_user is None:
+    print('This example cannot update admin user')
+else:
+    print(f'Update user {update_user.username} with id {update_user.id}')
+
+    update_user_payload = connection.users.config_to_update_payload(
+        update_user
+    )
+
+    update_user_payload.current_password = connection._config.password
+
+    new_password = 'Updated-passw0rd'
+
+    update_user_payload.password = new_password
+    update_user_payload.password_confirm = new_password
+
+    response = connection.users.update(update_user_payload)
+
+    print(type(response))
+    print(response)
